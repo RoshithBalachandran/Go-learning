98fb18423ce67d88d507ef66fd77327e37058fb1
RoshithBalachandran <roshithm9745@gmail.com>
2025-05-23 19:55:53 +0530

    To check the age while sending birth year

---
+++ Input/input.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	fmt.Println("Enter your birth year")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	input , _:= strconv.ParseInt(scanner.Text(),10,64)
+	fmt.Printf("Your age must be %d before 2025 end",2025-input)
+}
